fb56fa78567205288b3b3822a1d3194b24f6de22
Joanthan Liuti <liuti.john@gmail.com>
2011-06-09 16:53:28 +0200

    Added update-site feature + code refactoring

--- sword.py
+++ sword.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 #-*- coding: utf-8 -*-
 # Jonathan Liuti for Vox Teneo
 # Copyright (C) 2011 Jonathan Liuti
@@ -23,15 +24,20 @@
 
 import argparse, ConfigParser, json, os, pdb, urllib, tarfile, shutil,pwd, getpass,subprocess,MySQLdb,datetime, random, string, glob
 
-
-#######################
-# start creation here #
-#######################
-
-#init configuration
+#Init Configuration
 config = ConfigParser.ConfigParser()
 config.read('config.ini')
 current_env = config.get('general', 'env')
+
+#Utility Functions
+def get_database_list(mysql_host, mysql_user, mysql_password):
+   database_list_cmd="mysql -u %s -p%s -h %s --silent -N -e 'show databases'" % (mysql_user, mysql_password, mysql_host)
+   database_list = []
+   for database in os.popen(database_list_cmd).readlines():
+      database_list.append(database.strip())
+   return database_list
+
+#Main Functions
 def init_site(args):
    if not os.path.isfile('default.vhost'):
       raise Exception('default.vhost file required and not found.')
@@ -41,6 +47,7 @@ def init_site(args):
       site = 'site_'+site
       sitedomain = config.get(site,current_env+'_domain_name')
       #Take svn url from argument or config file
+      svn_url = None
       if args.svn_url:
          svn_url = args.svn_url
       elif config.has_option(site,'svn_url'):
@@ -62,8 +69,8 @@ def init_site(args):
 
       #add the domain to the host file
       #TODO do it the clean python way
-      #  with open('/etc/hosts') as host:
-      os.system('echo "127.0.0.1\t\t%s" >> /etc/hosts' % sitedomain)
+      with open('/etc/hosts') as host:
+         os.system('echo "127.0.0.1\t\t%s" >> /etc/hosts' % sitedomain)
       #get the latest wordpress release and checkout the files from svn
       os.chdir(sitedir)
       if svn_url:   
@@ -96,13 +103,6 @@ def init_site(args):
       shutil.rmtree("wordpress")
       os.system('chown -R %s:%s .' % (config.get('apache','user'),config.get('apache','group')))
 
-def get_database_list(mysql_host, mysql_user, mysql_password):
-   database_list_cmd="mysql -u %s -p%s -h %s --silent -N -e 'show databases'" % (mysql_user, mysql_password, mysql_host)
-   database_list = []
-   for database in os.popen(database_list_cmd).readlines():
-      database_list.append(database.strip())
-   return database_list
-
 def init_database(args):
    
    mysql_user = config.get('mysql_'+args.mysql_host,'user')
@@ -116,6 +116,7 @@ def init_database(args):
          site_section = 'site_'+site
          db_name = config.get(site_section,'db_name')
          db_user = config.get(site_section, 'db_user')
+         #If password is already set up in the config keep it - otherwise generate it
          db_password = config.has_option(site_section,'db_password') and config.get(site_section, 'db_password') or (''.join(random.choice(string.ascii_letters + string.digits) for x in range(16)))
          if not db_name:
             print "No database name provided"
@@ -130,19 +131,17 @@ def init_database(args):
             if os.system(cmd) != 0:
                print "The init database failed."
             else:
-               print "Database init succesfull.\ndb_name:%s \nuser:%s \npassword:%s" % (db_name,db_user,db_password)
+               print "Database init succesfull.\ndb_name:%s \nuser:%s \npassword:(see config.ini)" % (db_name,db_user)
+
+               if not config.has_option(site_section,'db_password') or not config.get(site_section,'db_password'): 
+                  config.set(site_section,'db_password',db_password)
+                  with open('config.ini', 'w') as configfile:
+                     config.write(configfile)
+
    else:
       print "The init database failed."
-#TODO output the generated password to the config file instead of printing it in the console.
 
-
-def delete_sites(args):
-   print "Delete command occurs here"
-
-def update_wordpress(args):
-   print" update site x to version y"
-
-def export_database(args):  
+def backup_database(args):  
    
    mysql_section = 'mysql_'+args.source_server
    mysql_user = config.get(mysql_section,'user')
@@ -167,15 +166,15 @@ def export_database(args):
 #TODO add an option to automatically put the db under svn - commit and stuff.
 #            result_sed = os.system("sed -e 's/'"+sitedomain+"'/'domain_to_replace'/' "+filename+" > "+filename)
             if result != 0:
-               print "Export failed."
+               print "Backup failed."
             else:               
-               print "Export succesfull."
+               print "Backup succesfull."
          else:
             print "Database %s does not exist on %s" % (database, mysql_host)
    else:
       print "No database found in source %s" % (mysql_host)
 
-def import_database(args):
+def restore_database(args):
    site_section = 'site_'+args.site
    backupdir = os.path.expanduser(config.get('general','backupdir'))
    db_name = config.get(site_section, 'db_name')
@@ -195,7 +194,7 @@ def import_database(args):
    #Control user input
    while True:
       try:
-         db_number = int(raw_input("Database number to import: "))
+         db_number = int(raw_input("Database number to restore: "))
          if 0 <= db_number < len(backup_list):
             break
          else: 
@@ -203,18 +202,45 @@ def import_database(args):
       except ValueError:
          print "Error: please enter a number"
          continue
-   confirm = raw_input("Are you sure that you want to import %s on server %s (yes/no) " % (backup_list[db_number], mysql_host))
+   confirm = raw_input("Are you sure that you want to restore %s on server %s (yes/no) " % (backup_list[db_number], mysql_host))
    if confirm == 'yes' or confirm == 'y':
       result = os.system("gunzip < %s | sed -e 's/'domain_to_replace'/'%s'/g' | mysql -h %s -u %s -p%s %s" % (backup_list[db_number], sitedomain, mysql_host, mysql_user, mysql_password, db_name))
       if result != 0 :
-         print "Import failed"
+         print "Restore failed"
       else:
-         print "Import succesfull"
+         print "Restore succesfull"
    else:
-      print "Import failed"
+      print "Restore failed"
    
+def delete_sites(args):
+   print "Delete command occurs here"
+
 def synch_upload_datas(args):
    print "synch_upload_datas command occurs here"
+
+def update_site(args):
+   wwwdir = config.get('apache', 'wwwdir')
+   for site in args.sites:
+      site = 'site_'+site
+      sitedomain = config.get(site,current_env+'_domain_name')
+      sitedir = (os.path.expanduser(wwwdir)+os.sep+sitedomain).replace('//','/')     
+      os.chdir(sitedir)
+    
+      #TODO Implement a local system for keeping already downloaded wordpress release
+      if args.wp_version == 'latest':
+         wp_version = 'latest'
+      else: 
+         wp_version = "wordpress-"+args.wp_version
+      os.system('rm -rf wp-admin wp-includes')
+      urllib.urlretrieve('http://wordpress.org/'+wp_version+'.tar.gz','tmp.tar.gz')
+      tar = tarfile.open("tmp.tar.gz", "r:gz")
+      tar.extractall()
+      os.remove("tmp.tar.gz")
+
+      #TODO make this work ? shutil.move("wordpress/*",".")
+      os.system("mv wordpress/* .")
+      shutil.rmtree("wordpress")
+      os.system('chown -R %s:%s .' % (config.get('apache','user'),config.get('apache','group')))
 
 
 # ARGUMENT PARSER SECTION #
@@ -248,9 +274,19 @@ parser_init_site.add_argument('-s','--svn', dest='svn_url', default=None, help='
 
 parser_init_site.set_defaults(func=init_site)
 
-#define parser for the init-database command
+# define parser for the update-site command
+parser_update_site = subparsers.add_parser('update-site',help='Update selected site to another wordpress version (default:latest version)')
 
-parser_init_database = subparsers.add_parser('init-database',help='Initialize a database to your local mysql and optionally create user/permission accordingly.')
+parser_update_site.add_argument('sites', metavar='sites', choices=site_choices, type=str, nargs='+', help='The site\'s name as defined in the config file section eg: my-site')
+
+parser_update_site.add_argument('-v','--version', dest='wp_version', default='latest', help='The version of wordpress to update to. Default: %(default)s version')
+
+parser_update_site.set_defaults(func=update_site)
+
+
+#define parser for the init-db command
+
+parser_init_database = subparsers.add_parser('init-db',help='Initialize a database to your local mysql and optionally create user/permission accordingly.')
 
 parser_init_database.add_argument('sites', metavar='sites',choices=site_choices, type=str, nargs='+', help='The site you want to init the database for.')
 
@@ -260,23 +296,23 @@ parser_init_database.add_argument('--host', dest='mysql_host', required=True, ch
 
 parser_init_database.set_defaults(func=init_database)
 
-#define parser for the export-database command
-parser_export_database = subparsers.add_parser('export-database',help='export a database from source to backup folder')
+#define parser for the backup-db command
+parser_backup_database = subparsers.add_parser('backup-db',help='backup a database from source to backup folder')
 
-parser_export_database.add_argument('sites', metavar='sites', type=str, nargs='+', choices=site_choices, help='The name of the site from which you want to export the db (separated by spaces)')
+parser_backup_database.add_argument('sites', metavar='sites', type=str, nargs='+', choices=site_choices, help='The name of the site from which you want to backup the db (separated by spaces)')
 
-parser_export_database.add_argument('-s','--source-server', dest='source_server', required=True, help='Database(s) will be exported FROM this location')
+parser_backup_database.add_argument('-s','--source-server', dest='source_server', required=True, help='Database(s) will be backuped FROM this location')
 
-parser_export_database.set_defaults(func=export_database)
+parser_backup_database.set_defaults(func=backup_database)
 
-#define parser for the import-database command
-parser_import_database = subparsers.add_parser('import-database',help='import a database from file to destination server')
+#define parser for the restore-db command
+parser_restore_database = subparsers.add_parser('restore-db',help='Restore a database from file to destination server')
 
-parser_import_database.add_argument('site', metavar='site', type=str, help='The name of the site you want to import the db to.')
+parser_restore_database.add_argument('site', metavar='site', type=str, help='The name of the site you want to restore the db to.')
 
-parser_import_database.add_argument('-d','--destination-server', dest='destination_server', required=True, help='Database(s) will be imported to this location')
+parser_restore_database.add_argument('-d','--destination-server', dest='destination_server', required=True, help='Database(s) will be restored to this location')
 
-parser_import_database.set_defaults(func=import_database)
+parser_restore_database.set_defaults(func=restore_database)
 #parse arguments and call the related function
 args = parser.parse_args() 
 args.func(args)
